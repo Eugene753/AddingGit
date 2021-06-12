850f6373f983494d8501f80f97ecf1f0fb363c34
Eugene753 <Imarkadanov@gmail.com>
2021-06-12 15:05:19 -0400

    Adding functions

---
+++ src/com/company/TestingClass.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class TestingClass {
+
+    public static void main(String[] args) {
+        int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+        int sum1,sum2,sum3,sum4,sum5,sum6,sum7,sum8,sum9,sum10;
+
+        num1=2;
+        num2=3;
+        num3=4;
+        num4=5;
+        num5=6;
+        num6=7;
+        num7=8;
+        num8=9;
+        num9=10;
+        num10=11;
+
+
+        sum1=num1+num2;
+
+        sum2=num3+num4;
+
+        sum3=num5+num6;
+
+
+    }
+}
